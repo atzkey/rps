2712ec625bc1b23068e69bd3b2b729f189bc29ac
Denis Bakunovitch <bakunovitch@gmail.com>
2020-02-21 10:50:14 +0300

    Localising the kitsune-ken rules

--- test/services/rps_test.rb
+++ test/services/rps_test.rb
@@ -1,16 +1,16 @@
 class RpsTest < ActiveSupport::TestCase
   test 'it plays the game, defined by the rules' do
-    # A 狐拳 (kitsune-ken) game
+    # A Kitsune-ken game
     game = Rps.new({
-      '狐' => ['庄屋'],
-      '庄屋' => ['猟師'],
-      '猟師' => ['狐']
+      'fox' => ['mayor'],
+      'mayor' => ['hunter'],
+      'hunter' => ['fox']
     })
 
-    assert_equal :tie, game.match('狐', '狐')
-    assert_equal :loss, game.match('庄屋', '狐')
-    assert_equal :win, game.match('狐', '庄屋')
-    assert_nil game.match('何', '狐')
-    assert_nil game.match('狐', '何')
+    assert_equal :tie, game.match('fox', 'fox')
+    assert_equal :loss, game.match('mayor', 'fox')
+    assert_equal :win, game.match('fox', 'mayor')
+    assert_nil game.match('unknown', 'fox')
+    assert_nil game.match('fox', 'unknown')
   end
 end
